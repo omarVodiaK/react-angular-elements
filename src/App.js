cac19dc0e325eb7abe742bb6517206833ea74a96
omarVodiaK <omar.boudfoust@gmail.com>
2020-09-17 00:35:07 +0800

    angular modal component in react project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import logo from './logo.svg'
+import './App.scss'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [toggle, setToggle] = useState()
+	const modalRef = React.createRef()
+
+	useEffect(() => {
+		modalRef.current.addEventListener('dismiss', () => setToggle(undefined))
+	})
+	const openModal = () => {
+		setToggle(true)
+	}
+
+	const closeModal = () => {
+		setToggle(undefined)
+	}
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				{/* <sebng-tooltip>Tooltip Reference in ng-content</sebng-tooltip> */}
+				<button onClick={openModal}>open modal</button>
+				<img src={logo} className="App-logo" alt="logo" />
+			</header>
+			<modal-component
+				id="modal"
+				centered={true}
+				toggle={toggle}
+				ref={modalRef}
+			>
+				<h1 header="">header</h1>
+				<p body="">Modal Body</p>
+				<div footer="">
+					<button onClick={closeModal}>close modal</button>
+				</div>
+			</modal-component>
+		</div>
+	)
 }
 
-export default App;
+export default App
